849a3a6cc6675726009c3c079d28a81bb64e8acf
gams <gabriel.antonioms@gmail.com>
2021-06-29 19:30:04 -0300

    v 0.1 -> print all chr and return length

--- ft_printf.c
+++ ft_printf.c
@@ -6,13 +6,41 @@
 /*   By: gantonio <gantonio@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/27 16:00:54 by gantonio          #+#    #+#             */
-/*   Updated: 2021/06/27 16:17:30 by gantonio         ###   ########.fr       */
+/*   Updated: 2021/06/29 19:20:57 by gantonio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdarg.h>
+#include "ft_printf.h"
+#include <stdio.h>
+
+int ft_treat_input(char *str)
+{
+	int char_count;
+	int i;
+
+	char_count = 0;
+	i = 0;
+	while (str[i])
+	{
+		if(str[i] != '%')
+			char_count += ft_putchar(str[i]);
+		else
+			ft_putchar(str[i]);
+		i++;
+	}
+	return (char_count);
+}
 
 int	ft_printf(const char *str, ...)
 {
-	return(0);
+	int char_count;
+	char *str_copy;
+	va_list args;
+
+	str_copy = ft_strdup(str);
+	va_start(args, str);
+	char_count = ft_treat_input(str_copy);
+	va_end(args);
+   
+   return(char_count);   
 }
--- main.c
+++ main.c
@@ -1,103 +1,85 @@
-#include <stdarg.h>
-#include "libft/libft.h"
+#include "ft_printf.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-int printf_new(char *pszFormatString, ...);
-int printf_manual(char *pszFormatString, ...);
+// int printf_new(char *pszFormatString, ...);
+// int printf_manual(char *pszFormatString, ...);
 
 int  main(int argc, char *argv[])
 {
-	printf("\nprintf:\n");
-        printf("%s",argv[0]);
-	printf("\nHello\n");
-	printf("Test String %s with integer %i and %% test\n", argv[0], argc);
-        printf("My name is %s, and I have %i yo.\n", "gamZs", 22);
+	int i;
+	int ft_i;
+	// printf("\nprintf:\n");
+    // printf("%s",argv[0]);
+	// printf("\nHello\n");
+	// printf("Test String %s with integer %i and %% test\n", argv[0], argc);
+    // printf("My name is %s, and I have %i yo.\n", "gamZs", 22);
 
-	printf_new("\n***********************************\n");
+	// printf("\n***********************************\n");
 
-	printf("\nprintf_new:\n");
-	printf_new(argv[0]); //its not equal
-	printf_new("\nHello\n");
-	printf_new("Test String %s with integer %i and %% test\n", argv[0], argc);
-        printf_new("My name is %s, and I have %i yo.\n", "gamZs", 22);
-	printf_new("\n***********************************\n");
+    // printf("\nft_printf:\n");
+    // ft_printf("Hello\n", "test");
+
+	i = printf("afg%d\n", 5);
+	printf("return printf: %d\n", i);
+
+	ft_i = ft_printf("afg%d\n", 5);
+	printf("return ft_printf: %d\n", ft_i);
    
-   return 0;
 }
 
-int  printf_new(char *pszFormatString, ...)
-{
-   int CharacterCount = 0;
-   int PrintInteger;
-   char *IntegerString;
-   char *pPrintString;
-   va_list VaList;
-
-   va_start(VaList, pszFormatString);
-   
-   while(*pszFormatString)
-   {
-       if(*pszFormatString == '%')
-       {
-           pszFormatString++;
-           switch(*pszFormatString)
-           {
-              case 's': 
-                      pPrintString = va_arg(VaList, char *);
-                      fputs(pPrintString, stdout);
-                      pszFormatString++;
-                      CharacterCount += strlen(pPrintString);
-                      break;
-              case 'i':  
-                      PrintInteger = va_arg(VaList, int);
-                      IntegerString = ft_itoa(PrintInteger);
-                      fputs(IntegerString, stdout);
-                      pszFormatString++;
-                      CharacterCount += strlen(IntegerString);
-                      break;
-              case '%': 
-                      putchar('%');
-                      pszFormatString++;
-                      CharacterCount++;
-                      break;
-              case '\0': 
-                      break;
-              default:
-                      putchar('%');
-                      putchar(*pszFormatString);
-                      pszFormatString++;            
-                      CharacterCount +=2;
-           }       
-       }
-       else
-       {
-           putchar(*pszFormatString);
-           pszFormatString++;
-           CharacterCount++;
-       }
-   }
-
-   va_end(VaList);
-
-   return CharacterCount;
-}
-
-// #include <stdio.h>
-// #include <stdlib.h>
-
-// int main ()
+// int  printf_new(char *pszFormatString, ...)
 // {
-//   int i;
-//   char buffer [33];
-//   printf ("Enter a number: ");
-//   scanf ("%d",&i);
-//   itoa (i,buffer,10);
-//   printf ("decimal: %s\n",buffer);
-//   itoa (i,buffer,16);
-//   printf ("hexadecimal: %s\n",buffer);
-//   itoa (i,buffer,2);
-//   printf ("binary: %s\n",buffer);
-//   return 0;
+//    int CharacterCount = 0;
+//    int PrintInteger;
+//    char *IntegerString;
+//    char *pPrintString;
+//    va_list VaList;
+
+//    va_start(VaList, pszFormatString);
+   
+//    while(*pszFormatString)
+//    {
+//        if(*pszFormatString == '%')
+//        {
+//            pszFormatString++;
+//            switch(*pszFormatString)
+//            {
+//               case 's': 
+//                       pPrintString = va_arg(VaList, char *);
+//                       fputs(pPrintString, stdout);
+//                       pszFormatString++;
+//                       CharacterCount += strlen(pPrintString);
+//                       break;
+//               case 'i':  
+//                       PrintInteger = va_arg(VaList, int);
+//                       IntegerString = ft_itoa(PrintInteger);
+//                       fputs(IntegerString, stdout);
+//                       pszFormatString++;
+//                       CharacterCount += strlen(IntegerString);
+//                       break;
+//               case '%': 
+//                       putchar('%');
+//                       pszFormatString++;
+//                       CharacterCount++;
+//                       break;
+//               case '\0': 
+//                       break;
+//               default:
+//                       putchar('%');
+//                       putchar(*pszFormatString);
+//                       pszFormatString++;            
+//                       CharacterCount +=2;
+//            }       
+//        }
+//        else
+//        {
+//            putchar(*pszFormatString);
+//            pszFormatString++;
+//            CharacterCount++;
+//        }
+//    }
+
+//    va_end(VaList);
+
+//    return CharacterCount;
 // }
