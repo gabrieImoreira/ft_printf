153e6e82d62f408dec8a71b488aa2ff67fb1a49e
gams <gabriel.antonioms@gmail.com>
2021-07-09 18:08:52 -0300

    v3.1 - print with porcent

--- ft_print_porcent.c
+++ ft_print_porcent.c
@@ -6,7 +6,7 @@
 /*   By: gantonio <gantonio@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/09 17:59:14 by gantonio          #+#    #+#             */
-/*   Updated: 2021/07/09 18:04:06 by gantonio         ###   ########.fr       */
+/*   Updated: 2021/07/09 18:08:35 by gantonio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,6 @@ int	ft_print_porcent(t_flags flags)
 	int counter;
 
 	counter = 0;
-	if (flags.minus == 1)
-		counter += ft_putstr("%", 1);
-	counter += ft_treat_width(flags.width, 1, flags.zero);
-	if (flags.minus == 0)
 		counter += ft_putstr("%", 1);
 	return (counter);
 }
