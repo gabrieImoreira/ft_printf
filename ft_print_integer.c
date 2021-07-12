801b6625b5f1fe6d14bb8cf114712874311aaf98
gams <gabriel.antonioms@gmail.com>
2021-07-12 00:45:08 -0300

    v4.3 - print integer with flags(2 functions)

--- ft_print_integer.c
+++ ft_print_integer.c
@@ -6,41 +6,51 @@
 /*   By: gantonio <gantonio@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/10 18:10:42 by gantonio          #+#    #+#             */
-/*   Updated: 2021/07/11 13:06:45 by gantonio         ###   ########.fr       */
+/*   Updated: 2021/07/12 00:34:06 by gantonio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include <stdio.h>
 
+static int	ft_putzero(char *str, t_flags flags)
+{
+	int		i;
+	char	*str_dot;
+	int		counter;
+
+	i = -1;
+	str_dot = malloc(sizeof(char) * ft_strlen(str) + 1);
+	ft_memcpy(str_dot, str, ft_strlen(str));
+	ft_bzero(str, ft_strlen(str));
+	while (++i <= (int)(flags.dot - (flags.width - ft_strlen(str_dot) + 1)))
+		str[i] = '0';
+	ft_memmove(str + i, str_dot, ft_strlen(str_dot) + 1);
+	counter = ft_strlen(str);
+	free(str_dot);
+	return (counter);
+}
+
 int	ft_print_integer(int integer, t_flags flags)
 {
 	int		counter;
-	int		zero_flag;
+	int		f_zero;
 	char	*str;
-	char	*str_dot;
-	int		i;
 
-	i = -1;
-	zero_flag = 0;
+	f_zero = 0;
 	counter = 0;
 	str = ft_itoa(integer);
-	if (flags.dot > 0 && flags.dot > ft_strlen(str))
-	{
-		ft_memcpy(str_dot, str, ft_strlen(str));
-		ft_bzero(str, ft_strlen(str));
-		while (++i <= (int)(flags.dot - (flags.width - ft_strlen(str_dot) + 1)))
-			str[i] = '0';
-		ft_memmove(str + i, str_dot, ft_strlen(str_dot) + 1);
-	}
+	if (flags.dot > 0 && flags.dot > (int)ft_strlen(str))
+		counter += ft_putzero(str, flags);
 	if (flags.zero == 1)
-		zero_flag = 1;
+		f_zero = 1;
 	if (flags.minus == 1)
 		ft_putstr(str, ft_strlen(str));
 	if (flags.dot > 0)
 		counter += ft_print_width((flags.width - ft_strlen(str) + 1), 1, 0);
 	else
-		counter += ft_print_width(flags.width - ft_strlen(str) + 1, 1, zero_flag);
+		counter += ft_print_width(flags.width - ft_strlen(str) + 1, 1, f_zero);
 	if (flags.minus == 0)
 		ft_putstr(str, ft_strlen(str));
+	return (counter);
 }
